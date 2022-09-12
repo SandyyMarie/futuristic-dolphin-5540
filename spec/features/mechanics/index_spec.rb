c20efc5a71b80c200b51e96ba76f4cb0381e506b
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-12 11:26:53 -0600

    test: added test for mechanics index page

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'As a User, when I visit the Mechanics Index Page' do
+    before :each do
+        @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+        @universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
+
+        @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+        @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+        @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+
+        @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+
+        @lisa = Mechanic.create!(name: "Lisa Simmons", years_of_experience: 11)
+        @jason = Mechanic.create!(name: "Jason Mayor", years_of_experience: 5)
+    end
+
+    it 'shows a header saying "All Mechanics" with a list of all mechanics names and their experience' do
+        visit "/mechanics"
+        expect(page).to have_content("Lisa Simmons, 11 years of experience")
+        expect(page).to have_content("Jason Mayor, 5 years of experience")
+    end
+
+    it 'also shows the average years of experience across all mechanics' do
+        visit "/mechanics"
+        expect(page).to have_content("Combined, their average years of experience is 8 years")
+    end
+end
